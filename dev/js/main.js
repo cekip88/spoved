7332cf400db4c3ed2496cd7232a85ec7ad8d9daf
Ruslan <cekip@mail.ru>
2020-12-23 16:29:53 +0600

    faq page ready

--- dev/js/main.js
+++ dev/js/main.js
@@ -12,6 +12,20 @@ class Front {
 				})
 			})
 		}
+		if(document.querySelector(".fq")) {
+			let btns = document.querySelectorAll('.fq .item-btn');
+			btns.forEach(function(btn) {
+				btn.addEventListener('click',function(e) {
+					e.preventDefault();
+					_.fqShow({
+						'event' : e, 'item' : this
+					})
+				})
+			})
+		}
+		window.addEventListener('resize',function() {
+			_.fqHeightCheck();
+		});
 		_.init();
 	}
 	
@@ -96,10 +110,49 @@ class Front {
 		return temp;
 	}
 	
-	
+	fqShow(clickData){
+		const _ = this;
+		let fqBlock = document.querySelector('.fq');
+		if(fqBlock) {
+			let block = clickData.item.parentElement;
+			let list = block.querySelector('.item-list');
+			block.classList.toggle('active');
+			if(block.classList.contains('active')){
+				let height = list.getAttribute('data-height');
+				list.setAttribute('style',`height:${height}px`);
+			} else {
+				list.removeAttribute('style')
+			}
+		}
+	}
+	fqHeightCheck() {
+		const _ = this;
+		let fqBlock = document.querySelector('.fq');
+		if(fqBlock) {
+			let lists = fqBlock.querySelectorAll('.item-list');
+			lists.forEach(function(list) {
+				let maxHeight = 0;
+				let items = list.childNodes;
+				items.forEach(function(item) {
+					let h = 0;
+					let inners = item.childNodes;
+					inners.forEach(function(inner) {
+						if(!isNaN(inner.offsetHeight)) {
+							h += inner.offsetHeight;
+							window.innerWidth < 1200 ? h += 10 : h+= 15;
+						}
+					});
+					if(h > maxHeight) maxHeight = h;
+				});
+				if(items.length > 3) maxHeight += 20;
+				list.setAttribute('data-height',`${maxHeight}`)
+			})
+		}
+	}
 	
 	init(){
 		const _ = this;
+		_.fqHeightCheck();
 	}
 }
 let front = new Front();
